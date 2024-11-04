c5a533307e1b8314d1871a2b92f7137d313a2cd5
Tenzing <163120613+sherpaten@users.noreply.github.com>
2024-11-04 11:47:54 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import GreetingElement from './myGreatingApp';
+
+function App() {
+  var element=<h1>
+    This is react
+  </h1>
+
+      return (
+        <div className="App">
+ 
+        </div>
+      );
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import GreetingElement from './myGreatingApp';
+import GreetingElementwithProp from './myGreetingprop';
+import AppColor from './AppbackgroundColor';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AppColor heading="This is first element" lbl="Name :" color="green"/>
+    <AppColor heading="This is second element" lbl="Name :" color="blue"/>
+    <AppColor heading="This is third third element" lbl="Name :" color="Yellow"/>
+  </React.StrictMode>
+
+
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
